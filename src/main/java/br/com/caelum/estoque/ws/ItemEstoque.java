5fc8524adabd06bf1517644f5249c16b60c02097
alexandregama <alexandre.gama.lima@gmail.com>
2015-07-14 20:59:11 -0300

    Adiciona Endpoint para buscar um Item do Estoque

---
+++ src/main/java/br/com/caelum/estoque/ws/ItemEstoque.java
@@ -0,0 +1,33 @@
+package br.com.caelum.estoque.ws;
+
+public class ItemEstoque {
+
+	private String codigo;
+	
+	private int quantidade;
+
+	ItemEstoque() {
+	}
+	
+	public ItemEstoque(String codigo, int quantidade) {
+		this.codigo = codigo;
+		this.quantidade = quantidade;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+}

842526f455e6a09bf278e7adb144502e43fdbe12
alexandregama <alexandre.gama.lima@gmail.com>
2015-07-28 22:36:14 -0300

    Cria endpoint para o envio de um novo pagamento a partir de uma transacao

--- src/main/java/br/com/caelum/payfast/PagamentoResource.java
+++ src/main/java/br/com/caelum/payfast/PagamentoResource.java
@@ -1,15 +1,20 @@
 package br.com.caelum.payfast;
 
 import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.ejb.Singleton;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/pagamentos")
 @Singleton
@@ -29,6 +34,18 @@ public class PagamentoResource {
 	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	public Pagamento buscaPagamento(@PathParam("id") Integer id) {
 		return repositorio.get(id);
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response criaPagamento(Transacao transacao) throws URISyntaxException {
+		Pagamento pagamento = new Pagamento(id++, transacao.getValor());
+		repositorio.put(pagamento.getId(), pagamento);
+		
+		return Response.created(new URI("/pagamentos/" + pagamento.getId()))
+				.entity(pagamento)
+				.type(MediaType.APPLICATION_JSON_TYPE)
+				.build();
 	}
 	
 }

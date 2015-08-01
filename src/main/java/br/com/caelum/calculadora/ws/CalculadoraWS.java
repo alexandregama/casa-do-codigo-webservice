03f98d8e8fb376e8fab2ddb24513becc7667c602
alexandregama <alexandre.gama.lima@gmail.com>
2015-08-01 06:36:10 -0300

    Melhora o nome dos parametros e metodos que aparecerão no WSDL

--- src/main/java/br/com/caelum/calculadora/ws/CalculadoraWS.java
+++ src/main/java/br/com/caelum/calculadora/ws/CalculadoraWS.java
@@ -1,13 +1,14 @@
 package br.com.caelum.calculadora.ws;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
 public class CalculadoraWS implements Calculadora {
 
-	@WebMethod
-	public int soma(int valor1, int valor2) {
+	@WebMethod(operationName = "somar")
+	public int soma(@WebParam(name = "valor1") int valor1, @WebParam(name = "valor2") int valor2) {
 		return valor1 + valor2;
 	}
 

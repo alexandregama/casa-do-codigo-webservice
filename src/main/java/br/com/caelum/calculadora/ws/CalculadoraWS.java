09641d53bccfa5b9b1f369e2d8da311b1b0c577a
alexandregama <alexandre.gama.lima@gmail.com>
2015-08-01 06:40:55 -0300

    Melhora o nome do retorno do método para o WSDL

--- src/main/java/br/com/caelum/calculadora/ws/CalculadoraWS.java
+++ src/main/java/br/com/caelum/calculadora/ws/CalculadoraWS.java
@@ -2,11 +2,13 @@ package br.com.caelum.calculadora.ws;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 @WebService
 public class CalculadoraWS implements Calculadora {
 
+	@WebResult(name = "resultadoSoma")
 	@WebMethod(operationName = "somar")
 	public int soma(@WebParam(name = "valor1") int valor1, @WebParam(name = "valor2") int valor2) {
 		return valor1 + valor2;
